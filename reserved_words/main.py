018b05c771154ab07f1eb7ee6d80600051772423
Hjwasim <61841901+hjwasim@users.noreply.github.com>
2022-11-09 15:00:27 +0530

    `reserved_words` commit

---
+++ reserved_words/main.py
@@ -0,0 +1,12 @@
+import keyword
+
+print(keyword.kwlist)
+"""
+['False', 'None', 'True', '__peg_parser__', 'and', 'as',
+ 'assert', 'async', 'await', 'break', 'class', 
+ 'continue', 'def', 'del', 'elif', 'else', 'except', 
+ 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
+'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 
+'return', 'try', 'while', 'with', 'yield']
+"""
+print(len(keyword.kwlist))  # totally 36 keywords
